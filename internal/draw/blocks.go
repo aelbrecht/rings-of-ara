2304111e2789b4ed3ddf0ec108a2eb74c6b4006a
aelbrecht <aelbrechtrudolf@gmail.com>
2020-09-01 01:27:02 +0200

    removing old drawing functions

---
+++ internal/draw/blocks.go
@@ -0,0 +1,42 @@
+package draw
+
+import (
+	"github.com/hajimehoshi/ebiten"
+	"rings-of-ara/internal/world"
+)
+
+func BlockLayer(w *world.Model, screen *ebiten.Image) {
+
+	// draw visible chunks on screen
+	for _, coords := range w.Camera.VisibleChunks() {
+
+		chunk := w.Planet.Chunks[coords]
+		if chunk == nil {
+			continue
+		}
+
+		for i, block := range chunk.Data {
+
+			if block.Texture == nil {
+				continue
+			}
+
+			op := ebiten.DrawImageOptions{}
+
+			// adjust for flipped y axis
+			op.GeoM.Translate(0, -world.BlockSize)
+
+			// scale blocks 3x
+			op.GeoM.Scale(3, 3)
+
+			// get screen position of block
+			pos := world.CombineChunkBlockPosition2(coords, world.BlockIndexToPosition(i))
+			x, y := w.Camera.ToScreen(pos).ValuesFloat()
+			op.GeoM.Translate(x, y)
+
+			sub := block.Texture
+
+			screen.DrawImage(sub, &op)
+		}
+	}
+}
